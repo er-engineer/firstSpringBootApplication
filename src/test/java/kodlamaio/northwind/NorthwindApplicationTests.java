679883101c28b79f21dc4085a7420f8683eec255
Eren Memiş <erenmemish@hotmail.com>
2023-11-01 23:30:48 +0300

    Initial commit

---
+++ src/test/java/kodlamaio/northwind/NorthwindApplicationTests.java
@@ -0,0 +1,14 @@
+package kodlamaio.northwind;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
